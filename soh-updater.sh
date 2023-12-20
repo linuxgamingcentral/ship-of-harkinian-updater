94b5272b99bb54931a23c5b75cfbe88d0a837131
linuxgamingcentral <101075966+linuxgamingcentral@users.noreply.github.com>
2023-12-20 17:31:11 -0500

    make window less wide

--- soh-updater.sh
+++ soh-updater.sh
@@ -14,7 +14,7 @@ fi
 title="Ship of Harkinian Updater"
 
 menu() {
-	zenity --width 1000 --height 350 --list --radiolist --multiple --title "$title"\
+	zenity --width 900 --height 350 --list --radiolist --multiple --title "$title"\
 	--column "Select an Option" \
 	--column "Option" \
 	--column="Description"\
